85acfe6bc8901c7a3d5c0c1ca5134fcf2303a1f3
Petko Tsikov <tsikov@gmail.com>
2017-11-10 21:13:22 +0100

    Move vcr require and configuration into test_helper
    This is needed to be reused later.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
